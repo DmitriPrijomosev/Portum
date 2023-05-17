efc21784bd9f2c0c10dc716423c2538d2bdd0681
DmitriPrijomosev <dmitri@dezajno.ee>
2023-05-17 15:44:18 +0300

    Lehed Teenused + Hinnad

--- src/Pages/Kuidas-toimida.jsx
+++ src/Pages/Kuidas-toimida.jsx
@@ -1,12 +1,96 @@
 import { useTranslation } from "react-i18next";
+import backgroundContact from "../Assets/backgroundContact.png";
+import img3 from "../Assets/Portum3.png";
+import img4 from "../Assets/Portum4.png";
+import '../CSS/activities.css';
 
 function Kuidastoimida() {
 
 const { t } = useTranslation();
 
 return (
-		<div>Kuidas toimida uus</div>
-	);
+  <div>
+    <div
+      className="section1 contactSection1"
+      style={{ backgroundImage: `url(${backgroundContact})` }}
+    >
+      <div className="_container contactContainer">
+        <div className="section1_1">
+          <div className="section1_2">
+            <div className="section1_3 contactSection1_3">
+              <h3>Как действовать</h3>
+            </div>
+          </div>
+        </div>
+        <div className="section1_4">
+          <div className="section1_4_1">
+            <h3>{t("section1.3")}</h3>
+            <p>{t("section1.4")}</p>
+          </div>
+          <div className="section1_4_2">
+            <a href="tel:+37259180009">5918 0009</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="section2"></div>
+    <div className="_container activitiesSection3">
+      <div className="activitiesSection3Column">
+        <div className="activitiesSection3Row">
+          <div className="activitiesSection3RowItemLeft">
+            <img src={img3} alt="Portum Matusebüroo, lilled" />
+          </div>
+          <div className="activitiesSection3RowItemRight">
+            <div className="whatTitle">
+              <h2>Если близкий человек умер дома</h2>
+            </div>
+            <article className="wahtText">
+              <p>
+                Если человек умер в своем доме, незамедлительно позвоните по
+                телефону 112, чтобы вызвать скорую помощь. Бригада скорой помощи
+                зафиксирует факт смерти и выдаст справку, которая в наше время
+                может быть в электронном формате.
+              </p>
+              <p>
+                После получения свидетельства о смерти позвоните нам на
+                круглосуточный номер 372 5918 0009
+              </p>
+              <p>
+                Позвонить нам Вы можете и сразу - мы проконсультируем и подержим
+                вас, организовав все необходимые дальнейшие действия.
+              </p>
+            </article>
+          </div>
+        </div>
+        <div className="activitiesSection3Row">
+          <div className="activitiesSection3RowItemLeft">
+            <img src={img4} alt="Portum Matusebüroo, lilled" />
+          </div>
+          <div className="activitiesSection3RowItemRight">
+            <div className="whatTitle">
+              <h2>Смерть близкого в больнице или доме по уходу</h2>
+            </div>
+            <article className="wahtText">
+              <p>
+                Большинство смертей происходит в больнице или домах по уходу.
+                Эти организации обычно не занимаются похоронами, однако они
+                самостоятельно оформят свидетельство о смерти и обеспечат
+                первичную опеку над умершим до тех пор, пока ближайшие
+                родственники не выберут похоронное бюро, которое займется телом.
+              </p>
+              <p>
+                После получения информации о смерти близкого, позвоните на наш
+                круглосуточный телефон. Мы организуем доставку тела в морг и
+                обсудим все необходимые дальнейшие шаги для проведения достойных
+                похорон.
+              </p>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
 }
 
 export default Kuidastoimida;
--- src/Pages/Teenused.jsx
+++ src/Pages/Teenused.jsx
@@ -1,11 +1,215 @@
 import { useTranslation } from "react-i18next";
+import backgroundContact from '../Assets/backgroundContact.png'
+import '../CSS/prices.css'
 
 function Teenused() {
 
 	const { t } = useTranslation();
 
 	return (
-		<div>Meie uued teenused</div>);
+    <div>
+      <div
+        className="section1 contactSection1"
+        style={{ backgroundImage: `url(${backgroundContact})` }}
+      >
+        <div className="_container contactContainer">
+          <div className="section1_1">
+            <div className="section1_2">
+              <div className="section1_3 contactSection1_3">
+                <h3>услуги и цены</h3>
+              </div>
+            </div>
+          </div>
+          <div className="section1_4">
+            <div className="section1_4_1">
+              <h3>{t("section1.3")}</h3>
+              <p>{t("section1.4")}</p>
+            </div>
+            <div className="section1_4_2">
+              <a href="tel:+37259180009">5918 0009</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="section2"></div>
+      <div className="pricesSection2row">
+        <div className="pricesSection2">
+          <h1 className="pricesSectionHeader">Организация похорон</h1>
+          <div className="pricesSection2TerxtArea">
+            <p>
+              С нашей помощю Вы сможете подобрать все необходимые ритуальные
+              пренадлежности, спланировать и провести похороны (гражданская
+              панихида или церковный обряд), а также траурно оформить место
+              захоронения. Наши опытные специалисты оперативно и максимально
+              корректно для родственников и близких покойного возьмут на себя
+              все заботы по организации и проведению этого траурного
+              мероприятия.
+            </p>
+            <p>
+              Каждые похороны, как и каждый ушедший близкий - индивидуальны.
+              Поэтому Вы можете выбрать один из готовых пакетов услуг или
+              выбрать то, что неободимо именно вам. Все детали мы можем обсудить
+              в любое удобное для вас время.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="pricesSection3">
+        <div className="_container pricesSection3Items">
+          <div className="pricesSection3Title">
+            <h2>Цены на пакеты</h2>
+          </div>
+          <div className="pricesSection3_row">
+            <div className="pricesSection3_column">
+              <div className="pricesSection3_column_item">
+                <div className="itemLabel">
+                  <h3>Индивидуальный</h3>
+                </div>
+                <div className="itemPrice">от 490 евро</div>
+                <ul className="itemList">
+                  <li className="itemListDescription">
+                    Все услуги по договорённости. Составте собственный,
+                    подходящий именно вам пакет
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="pricesSection3_column">
+              <div className="pricesSection3_column_item">
+                <div className="itemLabel">
+                  <h3>Рациональный</h3>
+                </div>
+                <div className="itemPrice">от 740 евро</div>
+                <ul className="itemList">
+                  <li className="itemListDescription">
+                    Гроб, обшитый тканью, драпировка
+                  </li>
+                  <li className="itemListDescription">Подушка шёлковая</li>
+                  <li className="itemListDescription">Покрывало шёлковое</li>
+                  <li className="itemListDescription">Ручки на гроб</li>
+                  <li className="itemListDescription">
+                    Хранение и туалет усопшего в морге
+                  </li>
+                  <li className="itemListDescription">Катафалк</li>
+                  <li className="itemListDescription">Организация похорон</li>
+                </ul>
+              </div>
+            </div>
+            <div className="pricesSection3_column">
+              <div className="pricesSection3_column_item">
+                <div className="itemLabel">
+                  <h3>Премиум</h3>
+                </div>
+                <div className="itemPrice">от 1100 евро</div>
+                <ul className="itemList">
+                  <li className="itemListDescription">
+                    Гроб, обшитый тканью, драпировка
+                  </li>
+                  <li className="itemListDescription">Подушка шёлковая</li>
+                  <li className="itemListDescription">Покрывало шёлковое</li>
+                  <li className="itemListDescription">Ручки на гроб</li>
+                  <li className="itemListDescription">
+                    Хранение и туалет усопшего в морге
+                  </li>
+                  <li className="itemListDescription">Катафалк</li>
+                  <li className="itemListDescription">Организация похорон</li>
+                  <li className="itemListDescription">Носильщики</li>
+                  <li className="itemListDescription">Свечи</li>
+                  <li className="itemListDescription">
+                    Временная пластиковая табличка на захоронение
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pricesSection4">
+        <div className="pricesSection4Items">
+          <div className="pricesSection3Title">
+            <h2>цены на отдельные услуги</h2>
+          </div>
+          <div className="pricesTable">
+            <table>
+              <tr>
+                <th>Наименование услуги</th>
+                <th>Цена</th>
+              </tr>
+              <tr>
+                <td>Круглосуточная транспортировка умершего в морг</td>
+                <td> бесплатно</td>
+              </tr>
+              <tr>
+                <td>Хранение в морге и туалет покойного</td>
+                <td>200€</td>
+              </tr>
+              <tr>
+                <td>Катафалк</td>
+                <td>60€</td>
+              </tr>
+              <tr>
+                <td>Услуги чтеца</td>
+                <td>от 120€</td>
+              </tr>
+              <tr>
+                <td>Услуги батюшки для отпевания в часовне или на кладбище</td>
+                <td>по договоренности</td>
+              </tr>
+              <tr>
+                <td>Широкий ассортимент гробов</td>
+                <td>от 200€ до 3 000€</td>
+              </tr>
+              <tr>
+                <td>Урны для кремации</td>
+                <td>от 55€</td>
+              </tr>
+              <tr>
+                <td>Доставка усопшего в крематорий в день кремации</td>
+                <td>70€</td>
+              </tr>
+              <tr>
+                <td>Композиция на крышку гроба, отрезные цветы</td>
+                <td>от 120€</td>
+              </tr>
+              <tr>
+                <td>
+                  Возможность заказа автобуса для перевозки людей 14-18 человек
+                </td>
+                <td>60€ / 1 час</td>
+              </tr>
+              <tr>
+                <td>Венки ,отрезные цветы, ветки, ели, зелень</td>
+                <td>от 60€</td>
+              </tr>
+              <tr>
+                <td>Бронирование места проведения поминок</td>
+                <td>По договоренности</td>
+              </tr>
+              <tr>
+                <td>Музыка по договорённости</td>
+                <td>oт 100€</td>
+              </tr>
+              <tr>
+                <td>
+                  Выезд ритуального агента на дом для консультации по
+                  организации похорон
+                </td>
+                <td>55€</td>
+              </tr>
+              <tr>
+                <td>Временная пластиковая табличка на захоронение</td>
+                <td>55€</td>
+              </tr>
+              <tr>
+                <td>Транспортировка усопшего из-за границы</td>
+                <td>по договоренности</td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Teenused;
